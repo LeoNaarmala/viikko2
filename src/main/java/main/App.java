80818da6e4ee19fbefc6dbcb3070c811d6b9d121
Leo <leo.naarmala@student.lut.fi>
2024-01-22 15:10:48 +0200

    modified:   src/main/java/main/App.java 	modified:   target/classes/main/App.class

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -6,6 +6,6 @@ public class App
     public static void main( String[] args )
     {
         Hedgehog hedgehog = new Hedgehog();
-        hedgehog.speak("moi");
+        hedgehog.speak();
     }
 }
